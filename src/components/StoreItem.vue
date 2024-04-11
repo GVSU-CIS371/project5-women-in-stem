e584dd9dbd56f83bd4f2bc1ec5cfd2238f25ba1d
pokey136 <pbernreu@gmail.com>
2024-04-10 23:30:58 -0400

    Progress on StoreItem

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,7 +1,24 @@
 <template>
-
+  <!-- your answer -->
+  <div>
+    <v-card>
+      <v-card-title>{{ props.product.data.name }}</v-card-title>
+      <v-card-subtitle>{{props.product.data.rating }}</v-card-subtitle>
+      <v-img :src="props.product.data.image" height="300"></v-img>
+      <v-card-text>{{ props.product.data.description }}</v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
+// your answer
+import { defineProps } from 'vue';
+import { ProductDoc } from '../types/product';
 
+const props = defineProps({
+  product: {
+    type: Object as () => ProductDoc,
+    required: true
+  }
+});
 </script>
