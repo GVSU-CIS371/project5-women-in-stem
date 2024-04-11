a1c741fadf817be82037f450081542e0ae1fb6ee
pokey136 <pbernreu@gmail.com>
2024-04-11 14:41:51 -0400

    changed storeitem

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -2,23 +2,19 @@
   <!-- your answer -->
   <div>
     <v-card>
-      <v-card-title>{{ props.product.data.name }}</v-card-title>
-      <v-card-subtitle>{{props.product.data.rating }}</v-card-subtitle>
-      <v-img :src="props.product.data.image" height="300"></v-img>
-      <v-card-text>{{ props.product.data.description }}</v-card-text>
+      <v-card-title>{{ product.data.name }}</v-card-title>
+      <v-card-subtitle>{{product.data.rating }}</v-card-subtitle>
+      <v-img :src="product.data.image" height="300"></v-img>
+      <v-card-text>{{ product.data.description }}</v-card-text>
     </v-card>
   </div>
 </template>
 
 <script lang="ts" setup>
 // your answer
-import { defineProps } from 'vue';
 import { ProductDoc } from '../types/product';
 
-const props = defineProps({
-  product: {
-    type: Object as () => ProductDoc,
-    required: true
-  }
-});
+defineProps<{
+  product: ProductDoc
+}>();
 </script>
