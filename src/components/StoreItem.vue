3f4f79498437bc22c62a7860addcb5b95625ff84
pokey136 <pbernreu@gmail.com>
2024-04-11 22:45:56 -0400

    worked on style of storeitem

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -2,13 +2,13 @@
   <div>
     <v-card>
       <v-card-title>{{ product.data.name }}</v-card-title>
-      <v-row>
-            
-            <v-rating :model-value="product.data.rating" color="yellow-darken-1" readonly></v-rating>
-            <v-icon icon="mdi-cash" /> {{ product.data.price }}
-            <v-icon icon="mdi-package-variant-closed" />{{ product.data.stock }}
-          </v-row>
-      <v-img :src="product.data.image" height="300"></v-img>
+        <v-row align="center" >  
+          <v-rating :model-value="product.data.rating" color="orange-lighten-3" readonly size="16"></v-rating>
+          <v-icon icon="mdi-cash" color = "green-lighten-3" class="mr-2" /> {{ product.data.price }}
+          <v-icon icon="mdi-package-variant-closed" color = "light-blue-lighten-3" class="mr-2"/>{{ product.data.stock }}
+        </v-row>
+      <v-spacer></v-spacer>
+      <v-img :src="product.data.image" width="200" height="300" class="mx-auto"></v-img>
       <v-card-text>{{ product.data.description }}</v-card-text>
     </v-card>
   </div>
